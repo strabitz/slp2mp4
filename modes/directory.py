c7ea878da9818c7744a9c79ccc9d7d6c769d490b
Davis C <davisclaib@gmail.com>
2024-11-29 08:58:08 -0500

    Better parallel usage

--- modes/directory.py
+++ modes/directory.py
@@ -4,6 +4,7 @@
 import pathlib
 import tempfile
 import multiprocessing
+import os
 
 import video
 import util
@@ -12,34 +13,45 @@ import ffmpeg
 # TODO: Make multiprocessing more efficient
 
 
-def _render_directory(conf, directory: pathlib.Path):
-    Ffmpeg = ffmpeg.FfmpegRunner(conf)
-    files_str = sorted(directory.glob("*.slp"), key=util.natsort)
-    files = [pathlib.Path(f) for f in files_str]
-    if len(files) == 0:
-        return None
-    output = pathlib.Path(f"""./{("_").join(directory.parts)}.mp4""")
-    with tempfile.TemporaryDirectory() as videodir_str:
-        video_files = []
-        videodir = pathlib.Path(videodir_str)
-        for file in files:
-            video_output = pathlib.Path(f"{videodir.joinpath(file.stem)}.mp4")
-            video.render(conf, file, video_output)
-            video_files.append(video_output)
-        Ffmpeg.concat_videos(video_files, output)
-    return output
-
-
-def _get_directories(conf, directory: pathlib.Path):
-    outputs = [directory]
+def _get_replays_by_directory(directory: pathlib.Path):
+    files = sorted(directory.glob("*.slp"), key=util.natsort)
+    outputs = {}
+    if len(files) > 0:
+        outputs.update({directory: files})
     for child in directory.iterdir():
         if child.is_dir():
-            outputs.extend(_get_directories(conf, child))
+            outputs = outputs | _get_replays_by_directory(child)
     return outputs
 
 
 def gather(conf, path: pathlib.Path):
-    dirs = _get_directories(conf, path)
-    args = [(conf, d) for d in dirs]
+    Ffmpeg = ffmpeg.FfmpegRunner(conf)
+    # Creates temp files for rendering
+    replays = _get_replays_by_directory(path)
+    tmp_files_by_dir = {
+        directory: [
+            tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") for f in videos
+        ]
+        for directory, videos in replays.items()
+    }
+    args = [
+        (conf, replay, pathlib.Path(tmp.name))
+        for directory in replays
+        for (replay, tmp) in zip(replays[directory], tmp_files_by_dir[directory])
+    ]
+    # Renders videos
     pool = multiprocessing.Pool(conf["runtime"]["parallel"])
-    pool.starmap(_render_directory, args)
+    pool.starmap(video.render, args)
+    # Concatenates videos
+    outputs = []
+    for directory, tmp_files in tmp_files_by_dir.items():
+        # TODO: Make relative to `path`?
+        output = pathlib.Path(f"""./{("_").join(directory.parts)}.mp4""")
+        outputs.append(output)
+        Ffmpeg.concat_videos([pathlib.Path(t.name) for t in tmp_files], output)
+    # Cleans up temp files
+    for t1 in tmp_files_by_dir.values():
+        for t2 in t1:
+            t2.close()
+            os.unlink(t2.name)
+    return outputs
